d5764687a8041e14222248fb4aa70fac4e2ea92f
jblose <jamblose@gmail.com>
2018-06-26 11:51:49 -0400

    Intial checkin of vm configuration commands

---
+++ vm_centos01/configure.sh
@@ -0,0 +1,22 @@
+#update system OS
+sudo yum update
+
+#install docker
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce
+sudo systemctl start docker
+sudo docker run hello-world
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+#install git
+sudo yum install git
+
+#install Azure CLI on CentOS
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[azure-cli]\nname=Azure CLI\nbaseurl=https://packages.microsoft.com/yumrepos/azure-cli\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
+sudo yum install azure-cli
+
+#test Azure CLI
+az login 
