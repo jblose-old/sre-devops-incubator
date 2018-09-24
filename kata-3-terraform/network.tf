a51423ca5f964d72d808ff3a1e0a6e55533204a1
jblose <jamblose-github@yahoo.com>
2018-09-23 21:16:36 -0400

    migrated variable substitution

--- kata-3-terraform/network.tf
+++ kata-3-terraform/network.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "prd" {
-    name = "prd-jblose"
+    name = "prd-${var.az_suffix}"
     location = "eastus"
     
     tags {
@@ -8,14 +8,14 @@ resource "azurerm_resource_group" "prd" {
 }
 
 resource "azurerm_virtual_network" "prd" {
-    name = "prd-vn-jblose"
+    name = "prd-vn-${var.az_suffix}"
     address_space = ["172.31.0.0/16"]
     location = "${azurerm_resource_group.prd.location}"
     resource_group_name = "${azurerm_resource_group.prd.name}"
 }
 
 resource "azurerm_subnet" "prd" {
-  name                 = "prd-vn-sub01-jblose"
+  name                 = "prd-vn-sub01-${var.az_suffix}"
   resource_group_name  = "${azurerm_resource_group.prd.name}"
   virtual_network_name = "${azurerm_virtual_network.prd.name}"
   address_prefix       = "172.31.2.0/24"
@@ -35,12 +35,12 @@ resource "azurerm_public_ip" "prd" {
 }
 
 resource "azurerm_network_interface" "prd" {
-  name                = "prd-vn-sub01-nic-jblose"
+  name                = "prd-vn-sub01-nic-${var.az_suffix}"
   location            = "${azurerm_resource_group.prd.location}"
   resource_group_name = "${azurerm_resource_group.prd.name}"
 
   ip_configuration {
-    name                          = "prd-vn-sub01-nic-ipconf-jblose"
+    name                          = "prd-vn-sub01-nic-ipconf-${var.az_suffix}"
     subnet_id                     = "${azurerm_subnet.prd.id}"
     private_ip_address_allocation = "dynamic"
     public_ip_address_id = "${azurerm_public_ip.prd.id}"
